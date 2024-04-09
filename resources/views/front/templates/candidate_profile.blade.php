4af759873d4f2b0b5ed12521a6b408b09537be05
Jang Keyte <44996556+jangkeyte@users.noreply.github.com>
2024-04-09 19:12:41 +0700

    minor update

--- resources/views/front/templates/candidate_profile.blade.php
+++ resources/views/front/templates/candidate_profile.blade.php
@@ -1,44 +1,31 @@
 <div class="jobsearch_side_box jobsearch_box_candidate_info position-relative">
     <div class="jobsearch_candidate_info">
-        <span class="promotepof-badge position-absolute top-0 end-0"><i class="fa fa-star" title="Featured"></i></span>
-        <div class="urgntpkg-candv2 position-absolute top-0"><span>urgent</span></div>
+        <span class="promotepof-badge position-absolute top-0 end-0"><i class="fa fa-star" title="{{ __('Featured') }}"></i></span>
+        <div class="urgntpkg-candv2 position-absolute top-0"><span>{{ __('urgent') }}</span></div>
         <figure>
             @include('front/templates/image', ['image' => $candidate_single->photo, 'name' => $candidate_single->name])
         </figure>
         <h2><a>{{ $candidate_single->name }}</a></h2>
         <p>{{ $candidate_single->designation }}</p>
-        <p>Phone: {{ $candidate_single->phone }}</p>
-        <p>Sector: {{ $candidate_single->rCandidateSector->name }}</p>
-        <p>(Age: {{  $candidate_single->date_of_birth->format('Y-m-d H:i:s')->diff(date('Y-m-d H:i:s'))->y; }} years)</p> <span>{{ $candidate_single->address }}</span>
-        <small>Member Since, 4 December, 2017</small>
+        <p>{{ __('Phone') }}: {{ $candidate_single->phone }}</p>
+        <p>{{ __('Sector') }}: {{ $candidate_single->rCandidateSector->name }}</p>
+        <p>({{ __('Age') }}: {{ \Carbon\Carbon::parse($candidate_single->date_of_birth)->age }} {{ __('years') }})</p> <span>{{ $candidate_single->address }}</span>
+        <small>{{ __('Member Since') }}, {{ $candidate_single->created_at }}</small>
         <ul>
-            <li>
-                <a href="https://www.facebook.com/envato" data-original-title="facebook"
-                    class="jobsearch-icon jobsearch-facebook-logo"></a>
-            </li>
-            <li>
-                <a href="https://www.twitter%20.com/envato" target="_blank" data-original-title="twitter"
-                    class="jobsearch-icon jobsearch-twitter-logo"></a>
-            </li>
-            <li>
-                <a href="https://www.linkedin.com/envato" target="_blank" data-original-title="linkedin"
-                    class="jobsearch-icon jobsearch-linkedin-button"></a>
-            </li>
-            <li>
-                <a href="https://www.dribbble.com/envato" target="_blank" data-original-title="dribbble"
-                    class="jobsearch-icon jobsearch-dribbble-logo"></a>
-            </li>
+            <li><a href="{{ $candidate_single->facebook }}"><i class="px-2 fs-4 fa-brands fa-facebook-f fa-lg hover-color-danger"></i></a></li>
+            <li><a href="{{ $candidate_single->twitter }}"><i class="px-2 fs-4 fa-brands fa-twitter fa-lg hover-color-danger"></i></a></li>
+            <li><a href="{{ $candidate_single->pinterest }}"><i class="px-2 fs-4 fa-brands fa-google-plus fa-lg hover-color-danger"></i></a></li>
+            <li><a href="{{ $candidate_single->linkedin }}"><i class="px-2 fs-4 fa-brands fa-linkedin fa-lg hover-color-danger"></i></a></li>                    
         </ul>
-        <a href="javascript:void(0);" class="jobsearch-candidate-default-btn jobsearch-open-signin-tab"><i
-                class="jobsearch-icon jobsearch-add-list"></i> Save Candidate </a>
+        <!--
+        <a href="javascript:void(0);" class="jobsearch-candidate-default-btn jobsearch-open-signin-tab">
+            <i class="jobsearch-icon jobsearch-add-list"></i> Save Candidate </a>
         <div class="jobsearch-whatsapp-msgcon jobsearch_whatsap_view_1">
             <a href="https://wa.me/0979380948?text=Hi" target="_blank">
                 <i class="fa fa-whatsapp"></i>
                 <small>WhatsApp</small>
             </a>
         </div>
-        <div class="candinvite-btn-con">
-            <a href="javascript:void(0);" class="jobsearch-candinvite-btn jobsearch-open-signin-tab">Invite</a>
-        </div>
+        -->
     </div>
 </div>
